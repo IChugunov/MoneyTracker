15f4c475b7ca0bfc8d2af58646470da8ec29a403
IChugunov <ivan-chugunov@list.ru>
2017-06-27 15:09:33 +0300

    TabLayout with fragments in MainActivity, changed theme colors

--- app/src/main/java/com/ivanchug/moneytracker/MainActivity.java
+++ app/src/main/java/com/ivanchug/moneytracker/MainActivity.java
@@ -2,22 +2,62 @@ package com.ivanchug.moneytracker;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabs;
+    private ViewPager pages;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.items);
-        final RecyclerView items = (RecyclerView) findViewById(R.id.items);
-        items.setAdapter(new ItemsAdapter());
+        setContentView(R.layout.activity_main);
+
+        tabs = (TabLayout) findViewById(R.id.tabs);
+        pages = (ViewPager) findViewById(R.id.pages);
+
+        pages.setAdapter(new MainPagerAdapter());
+        tabs.setupWithViewPager(pages);
     }
 
+    private class MainPagerAdapter extends FragmentPagerAdapter {
+        private final String[] titles;
+        private final String[] types = {Item.TYPE_EXPENSE, Item.TYPE_INCOME};
 
 
+        public MainPagerAdapter() {
+            super(getSupportFragmentManager());
+            titles = getResources().getStringArray(R.array.main_pager_titles);
+        }
 
+        @Override
+        public Fragment getItem(int position) {
+            if (position == getCount() - 1)
+                return new BalanceFragment();
+
+            Bundle args = new Bundle();
+            final ItemsFragment itemsFragment = new ItemsFragment();
+            args.putString(ItemsFragment.ARG_TYPE, types[position]);
+            itemsFragment.setArguments(args);
+            return itemsFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+    }
 }
