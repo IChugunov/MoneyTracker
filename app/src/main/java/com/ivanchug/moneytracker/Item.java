66394d4af80a5bffeb7432652cbd3c1446981a31
IChugunov <ivan-chugunov@list.ru>
2017-06-29 14:39:39 +0300

    connected application to internet server

--- app/src/main/java/com/ivanchug/moneytracker/Item.java
+++ app/src/main/java/com/ivanchug/moneytracker/Item.java
@@ -1,18 +1,21 @@
 package com.ivanchug.moneytracker;
 
+import java.io.Serializable;
+
 /**
  * Created by Иван on 24.06.2017.
  */
 
-public class Item {
+public class Item implements Serializable {
     public static final String TYPE_EXPENSE = "expense";
     public static final String TYPE_INCOME = "income";
 
-    final String name;
-    final int price;
+    public String name, type;
+    public int id = -1, price;
 
-    public Item(String name, int price) {
+    public Item(String name, int price, String type) {
         this.name = name;
         this.price = price;
+        this.type = type;
     }
 }
---
+++ app/src/main/java/com/ivanchug/moneytracker/api/AddResult.java
@@ -0,0 +1,9 @@
+package com.ivanchug.moneytracker.api;
+
+/**
+ * Created by Иван on 29.06.2017.
+ */
+
+public class AddResult extends Result {
+    public int id;
+}
