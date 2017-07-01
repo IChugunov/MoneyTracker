2a74b01eda5d609067387b5288405a088c3f0e34
IChugunov <ivan-chugunov@list.ru>
2017-07-01 12:40:59 +0300

    added CardView to AddActivity layout file

--- app/src/main/java/com/ivanchug/moneytracker/ItemsFragment.java
+++ app/src/main/java/com/ivanchug/moneytracker/ItemsFragment.java
@@ -71,7 +71,7 @@ public class ItemsFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == AddActivity.RC_ADD_ITEM && resultCode == RESULT_OK) {
             Item item = (Item) data.getSerializableExtra(AddActivity.RESULT_ITEM);
-            Toast.makeText(getContext(), item.name + item.price, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), item.name + " " + item.price, Toast.LENGTH_SHORT).show();
         }
     }
 
